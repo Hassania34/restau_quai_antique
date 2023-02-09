a5772677dd9d5adcb72e61e2e54510002f017df8
Hassania <hassania@macbook-pro-de-hassania.home>
2023-02-09 11:20:00 +0100

    Page d'accueil / Page de connexion - Todo : carte / réservation / à propos

--- src/Controller/ConnexionController.php
+++ src/Controller/ConnexionController.php
@@ -5,16 +5,16 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class AccueilController extends AbstractController
+class ConnexionController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/connexion")
      */
-    public function accueil(): Response
+    public function connexion(): Response
     {
         $number = random_int(0, 100);
 
-        return $this->render('accueil.html.twig', [
+        return $this->render('connexion.html.twig', [
             'number' => $number,
         ]);
     }
