6320e788b74fc9f79a36551c28d899c15e5fb7bf
Hassania <hassania@macbook-pro-de-hassania.home>
2023-02-09 10:12:51 +0100

    Init restau_quai_antique symfony project

---
+++ src/Controller/AccueilController.php
@@ -0,0 +1,21 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class AccueilController extends AbstractController
+{
+    /**
+     * @Route("/accueil")
+     */
+    public function accueil(): Response
+    {
+        $number = random_int(0, 100);
+
+        return $this->render('accueil.html.twig', [
+            'number' => $number,
+        ]);
+    }
+}
