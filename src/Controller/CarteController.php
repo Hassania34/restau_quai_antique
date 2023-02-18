b8374f4cdb5afed01129cb5c77f2801822859b06
Hassania34 <hassania.hajjaj@armloccitanie.org>
2023-02-18 12:29:12 +0100

    Résolution du problème de l'affichage de la session sur la page carte

--- src/Controller/CarteController.php
+++ src/Controller/CarteController.php
@@ -4,11 +4,22 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class CarteController extends AbstractController
 {
-    public function carte(): Response
+    public function carte(Request $request): Response 
     {
+    
+        $session = $request->getSession();
+        $utilisateur = $session->get('utilisateur');
+        if (is_null($utilisateur)) {
+            return $this->render('carte.html.twig', [
+            ]);
+        }
+        return $this->render('carte.html.twig', [
+            'utilisateur' => $utilisateur->getUsername()
+        ]);
         return $this->render('carte.html.twig');
     }
 }
