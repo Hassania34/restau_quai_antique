eaf7642c0c9f17239d2097d295de97fb994f121b
Hassania34 <hassania.hajjaj@armloccitanie.org>
2023-02-18 12:12:02 +0100

    Initialisation du formulaire de réservation - Problème : la session utilisateur est perdue lorsqu'on quitte la page d'accueil

--- src/Controller/ReservationController.php
+++ src/Controller/ReservationController.php
@@ -1,6 +1,8 @@
 <?php 
 namespace App\Controller;
 
+use App\Entity\Reservation;
+use App\Form\Type\ReservationType;
 use symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,7 +13,13 @@ class ReservationController extends AbstractController
 
     public function reserver(Request $request): Response 
     {   
-        return $this->render('reservation.html.twig');
+        $reservation = new Reservation();
+        $form = $this->createForm(ReservationType::class, $reservation);
+        $form->handleRequest($request);
+
+        return $this->renderForm('reservation.html.twig', [
+            'form' => $form,
+        ]);
     }
   
 }
--- src/Form/Type/ReservationType.php
+++ src/Form/Type/ReservationType.php
@@ -4,25 +4,20 @@ namespace App\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TimeType;
 
 class ReservationType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nbcouvert', IntegerType::class)
+            ->add('nbcouverts', IntegerType::class)
             ->add('date', DateType::class)
             ->add('heure', TimeType::class)
-            ->add('allergies', TextareaType::class)
             ->add('save', SubmitType::class)
         ;
     }
-
-    // public function configureOptions(OptionsResolver $resolver): void
-    // {
-    //     $resolver->setDefaults([
-    //         // Configure your form options here
-    //     ]);
-    // }
 }
