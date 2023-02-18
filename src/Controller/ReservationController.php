f7f837ecc2d7b1c1f026d08c30c193f5c06a8701
Hassania34 <hassania.hajjaj@armloccitanie.org>
2023-02-18 12:14:46 +0100

    Résolution : la session utilisateur est perdue lorsqu'on quitte la page d'accueil

--- src/Controller/ReservationController.php
+++ src/Controller/ReservationController.php
@@ -16,9 +16,16 @@ class ReservationController extends AbstractController
         $reservation = new Reservation();
         $form = $this->createForm(ReservationType::class, $reservation);
         $form->handleRequest($request);
-
+        $session = $request->getSession();
+        $utilisateur = $session->get('utilisateur');
+        if (is_null($utilisateur)) {
+            return $this->renderForm('reservation.html.twig', [
+                'form' => $form,
+            ]);
+        }
         return $this->renderForm('reservation.html.twig', [
             'form' => $form,
+            'utilisateur' => $utilisateur->getUsername()
         ]);
     }
   
